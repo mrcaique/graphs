b0cb994fe8bb0561855a4028adbdc3685aeb5fb4
CaiqueMarques <caiquermarques95@gmail.com>
2015-10-17 23:06:28 -0300

    Modify methods

--- python/graph.py
+++ python/graph.py
@@ -1,41 +1,41 @@
 """
 	A class that represent a Graph G. This graph was constructed
 	with represent a dictionary of vertices and every vertex is
-	a dictionary.
+	a set.
 """
+#!/usr/bin/env python3
 from random import choice
 
 class Graph(object):
 	# Contructs a intance of the graph G
-	def __init__(self, vertices={}, directed=False, valued=False):
+	def __init__(self, vertices={}, directed=False):
 		self.vertices = vertices
 		self.directed = directed
-		self.valued = valued
 
 	# Add a vetex in the graph G
 	def add_vertex(self, vertex):
 		if vertex not in self.vertices:
-			self.vertices[vertex] = {}
+			self.vertices[vertex] = set()
 
 	# Remove a vertex in the graph G
 	def remove_vertex(self, vertex):
 		if vertex in self.vertices:
-			del self.vertices[vertex]
 			for v in self.vertices:
-				if vertex in v:
-					del v[vertex]
+				if vertex in self.vertices[v]:
+					self.vertices[v].remove(vertex)
+			del self.vertices[vertex]
 
 	# Connects (add a edge) between two given vertices
 	def connect(self, vertex1, vertex2):
 		if vertex1 in self.vertices and vertex2 in self.vertices:
-			self.vertices[vertex1] = vertex2
-			self.vertices[vertex2] = vertex1
+			self.vertices[vertex1] = [vertex2]
+			self.vertices[vertex2] = [vertex1]
 
 	# Disconnects (remove the edges) between two given vertices 
 	def disconnect(self, vertex1, vertex2):
 		if vertex1 in self.vertices and vertex2 in self.vertices:
-			del self.vertices[vertex1][vertex2]
-			del self.vertices[vertex2][vertex1]
+			self.vertices[vertex1].remove(vertex2)
+			self.vertices[vertex2].remove(vertex1)
 
 	# Shows the order of the graph G
 	def order(self):
@@ -43,8 +43,7 @@ class Graph(object):
 
 	# Get the set of the vertices of G
 	def get_vertices(self):
-		set_vertices = set()
-		set_vertices.add(self.vertices)
+		set_vertices = set(self.vertices.keys())
 		return set_vertices
 
 	# Return a random vertex
@@ -54,9 +53,8 @@ class Graph(object):
 	# Return a set with the vertex's adjacents
 	def get_adjacents(self, vertex):
 		set_adjacents = set()
-		set_adjacents.add(self.vertices[vertex])
 		for v in self.vertices:
-			if vertex in v:
+			if vertex in self.vertices[v]:
 				set_adjacents.add(v)
 		return set_adjacents
 
@@ -66,11 +64,7 @@ class Graph(object):
 
 	# Get the degree of reception of a given vertex
 	def get_vertex_indegree(self, vertex):
-		count = 0
-		for v in self.vertices:
-			if vertex in v:
-				count = count + 1
-		return count
+		return len(self.get_adjacents(vertex))
 
 	# Get the degree of a given vertex
 	def get_degree(self, vertex):
